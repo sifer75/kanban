586142291d6f2a3a605e120fa04006c79366d5cb
Fabien Taupin <taupinfabien2607@gmail.com>
2024-11-08 22:01:52 +0100

    automatisation des migrations

--- funb/start.sh
+++ funb/start.sh
@@ -1,15 +1,42 @@
-#!/bin/sh
+#! /bin/sh
 
-node ace make:migration users
+if [ -z "$DB_USER" ] || [ -z "$DB_PASSWORD" ] || [ -z "$DB_DATABASE" ]; then
+  echo "Les variables d'environnement DB_USER, DB_PASSWORD ou DB_DATABASE ne sont pas définies."
+  exit 1
+fi
 
-node ace make:migration workspaces
+until pg_isready -h postgres -U "$DB_USER" -d "$DB_DATABASE"; do
+  echo 'En attente de la base de donnée ...'
+  sleep 2
+done
 
-node ace make:migration kanbans
+export PGPASSWORD=${DB_PASSWORD}
 
-node ace make:migration tasks
+table_exists=$(psql "postgresql://${DB_USER}:${DB_PASSWORD}@postgres/${DB_DATABASE}" -t -c "
+SELECT EXISTS (
+  SELECT 1
+  FROM pg_tables
+  WHERE schemaname = 'public'
+  AND tablename = 'users'
+)" | xargs)
 
-node ace make:migration missions
 
-node ace migration:run
+echo "Valeur de table_exists: '$table_exists'"
+echo "DB_USER=$DB_USER, DB_PASSWORD=$DB_PASSWORD"
 
-node ./build/bin/server.js
+
+if [ "$table_exists" = "f" ]; then
+  echo 'Création des migrations ...'
+  node ace make:migration users
+  node ace make:migration workspaces
+  node ace make:migration kanbans
+  node ace make:migration tasks
+  node ace make:migration missions
+  node ace migration:run --force
+  echo 'Les migrations sont appliquées.'
+else
+  echo 'Les migrations existent déjà.'
+fi
+
+npm start
+
